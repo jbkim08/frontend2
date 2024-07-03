78762d515da34da0e61227392908e5d2451764bc
JBKIM <drv98@naver.com>
2024-07-03 11:35:07 +0900

    부트스트랩 네브바

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import "./App.css";
 import Navbar from "./layout/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import "./App.css";
 
 function App() {
   return (
