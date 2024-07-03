1c4e2992ca3628322e2a0a2fb298855587de4a27
JBKIM <drv98@naver.com>
2024-07-03 11:45:29 +0900

    홈페이지에 테이블

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
 import "./App.css";
+import Home from "./page/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
