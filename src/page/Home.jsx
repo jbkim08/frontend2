81bde3c40005f9a88de9eba2e5bdd3adfc93dcda
JBKIM <drv98@naver.com>
2024-07-04 09:38:51 +0900

    유저데이터 테이블 출력

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,35 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  //[{유저1},{유저2},{유저3}...]
+  const [users, setUsers] = useState([]);
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/api/users");
+    setUsers(result.data);
+  };
+  //처음 한번 유저데이터 가져오기
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
   return (
     <div className="container">
-      <table className="table border shadow my-4">
+      <table className="table border shadow text-center my-4">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">이름</th>
+            <th scope="col">유저네임</th>
+            <th scope="col">이메일</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {users &&
+            users.map((u, i) => (
+              <tr key={i}>
+                <th scope="row">{i + 1}</th>
+                <td>{u.name}</td>
+                <td>{u.username}</td>
+                <td>{u.email}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
